c64f9e5eef0438bd475c5bf7993ee6d21e4f7980
JongWasTaken <jong@smto.pw>
2024-08-14 17:07:27 +0200

    Fix broken mining speed

--- src/main/java/pw/smto/moretools/item/BaseToolItem.java
+++ src/main/java/pw/smto/moretools/item/BaseToolItem.java
@@ -1,15 +1,11 @@
 package pw.smto.moretools.item;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-import eu.pb4.polymer.core.api.item.PolymerItem;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.*;
 import net.minecraft.item.tooltip.TooltipType;
-import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
@@ -17,18 +13,18 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
+import pw.smto.moretools.Config;
+import pw.smto.moretools.MoreTools;
 import pw.smto.moretools.util.MutableMaterial;
 
 import java.util.List;
-import java.util.Map;
 
 public class BaseToolItem extends MiningToolItem {
-    private final float baseSpeed;
+    //private final float baseSpeed;
     public BaseToolItem(MiningToolItem baseItem, TagKey<Block> targetBlocks) {
         super(
                 // durability gets tripled
-                MutableMaterial.of(baseItem.getMaterial()).setDurability(baseItem.getDefaultStack().getMaxDamage() * 3),
+                MutableMaterial.of(baseItem.getMaterial()).setDurability((int) (baseItem.getDefaultStack().getMaxDamage() * Config.Data.DURABILITY_MULTIPLIER)),
                 // derive target blocks from baseItem
                 targetBlocks,
                 // damage and mining speed get nerfed
@@ -40,25 +36,23 @@ public class BaseToolItem extends MiningToolItem {
                         )
                 )
         );
-        this.baseSpeed = baseItem.getMaterial().getMiningSpeedMultiplier();
+        //this.baseSpeed = baseItem.getMaterial().getMiningSpeedMultiplier();
     }
 
     private boolean actAsBaseTool = false;
     @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        MoreTools.LOGGER.warn(String.valueOf(stack.get(DataComponentTypes.TOOL).defaultMiningSpeed()));
         if (selected)
         {
             if (entity instanceof ServerPlayerEntity serverPlayerEntity) {
                 if (serverPlayerEntity.isSneaking())
                 {
-                    stack.get(DataComponentTypes.TOOL).defaultMiningSpeed = this.baseSpeed;
+                    stack.get(DataComponentTypes.TOOL).defaultMiningSpeed = 1.0F;
                     this.actAsBaseTool = true;
                 }
                 else {
-                    stack.get(DataComponentTypes.TOOL).defaultMiningSpeed = this.baseSpeed - 3.0F;
-                    if (stack.get(DataComponentTypes.TOOL).defaultMiningSpeed() < 1.0F) {
-                        stack.get(DataComponentTypes.TOOL).defaultMiningSpeed = 1.1F;
-                    }
+                    stack.get(DataComponentTypes.TOOL).defaultMiningSpeed = 0.5F;
                     this.actAsBaseTool = false;
                 }
             }
@@ -68,6 +62,7 @@ public class BaseToolItem extends MiningToolItem {
     public String getGimmickText() {
         return "";
     }
+
     @Override
     public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType type) {
         tooltip.add(Text.literal(getGimmickText()).formatted(Formatting.GOLD));
